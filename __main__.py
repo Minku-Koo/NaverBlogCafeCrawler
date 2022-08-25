02293ac7f6011e895265609e2f5e2fa9467c371a
Ji-yong219 <wldydslapjyy@naver.com>
2022-08-25 20:40:50 +0900

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -8,10 +8,10 @@ where = "blog"
 
 result = getURLs(keyword, where, start_date, end_date)
 
-print(f"{'='*20} {where} URL {'='*20}")
-for url in result:
-    print(url)
-print(f"{'='*50}\n")
+# print(f"{'='*20} {where} URL {'='*20}")
+# for url in result:
+#     print(url)
+# print(f"{'='*50}\n")
 
 print(f"총 {len(result)}개 크롤링 완료")
 
