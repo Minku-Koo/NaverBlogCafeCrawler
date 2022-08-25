06be9fc9df3b4d014ba31062129a3dd69c1610da
Ji-yong219 <wldydslapjyy@naver.com>
2022-08-25 20:54:56 +0900

    update keywords

--- __main__.py
+++ __main__.py
@@ -1,18 +1,25 @@
 from crawler.NaverCrawler import getURLs
 from utils.FileControler import listToTXT
 
-keyword = "이불 관리+이불 건조+이불 먼지"
-start_date = "20190825"
-end_date = "20220825"
-where = "blog"
+keywords = [
+    "이불 관리",
+    "이불 건조",
+    "이불 먼지"
+]
+for keyword in keywords:
+    start_date = "20190825"
+    end_date = "20220825"
 
-result = getURLs(keyword, where, start_date, end_date)
+    where = "blog"
 
-# print(f"{'='*20} {where} URL {'='*20}")
-# for url in result:
-#     print(url)
-# print(f"{'='*50}\n")
+    result = getURLs(keyword, where, start_date, end_date)
+    listToTXT(result, f"{keyword}_{start_date}_{end_date}_{where}.txt")
+    print(f"{keyword} {where} 총 {len(result)}개 크롤링 완료")
 
-print(f"총 {len(result)}개 크롤링 완료")
 
-listToTXT(result, f"{keyword}_{start_date}_{end_date}_{where}.txt")
+    where = "cafe"
+
+    result = getURLs(keyword, where, start_date, end_date)
+    listToTXT(result, f"{keyword}_{start_date}_{end_date}_{where}.txt")
+    print(f"{keyword} {where} 총 {len(result)}개 크롤링 완료")
+
