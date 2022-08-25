87383549745f2e3fa75c098de90b217439a4c7e6
Ji-yong219 <wldydslapjyy@naver.com>
2022-08-25 19:08:23 +0900

    fix whole page issue

--- __main__.py
+++ __main__.py
@@ -1,20 +1,15 @@
 from crawler.NaverCrawler import getURLs
 
 keyword = "스타 빨무"
-start_date = "20210101"
+start_date = "20220601"
 end_date = "20220825"
+where = "cafe"
 
-result = getURLs(keyword, start_date, end_date)
+result = getURLs(keyword, where, start_date, end_date)
 
-print(f"{'='*20} 카페 URL {'='*20}")
-for cafe in result.get("cafe_urls"):
-    print(cafe)
+print(f"{'='*20} {where} URL {'='*20}")
+for url in result:
+    print(url)
 print(f"{'='*50}\n")
 
-print(f"{'='*20} 블로그 URL {'='*20}")
-for cafe in result.get("blog_urls"):
-    print(cafe)
-print(f"{'='*50}\n")
-print()
-
-print(f"총 {result.get('count')}개 크롤링 완료")
+print(f"총 {len(result)}개 크롤링 완료")
--- crawler/NaverCrawler.py
+++ crawler/NaverCrawler.py
@@ -4,48 +4,42 @@ from bs4 import BeautifulSoup as bs
 
 def getURLs(
             keyword,
+            where,
             start_date,
             end_date
         ):
-        
+
     page = 1
     a_tags = []
 
     while True:
-        URL = f"""https://s.search.naver.com/p/review/search.naver?rev=44&where=view&api_type=11&start={page}&query={keyword}&nso=so:r,p:from{start_date}to{end_date},a:all&main_q=&mode=normal&q_material=&ac=0&aq=0&spq=0&st_coll=&topic_r_cat=&nx_search_query=&nx_and_query=&nx_sub_query=&prank=31&sm=tab_opt&ssc=tab.view.view&ngn_country=KR&lgl_rcode=02465101&fgn_region=&fgn_city=&lgl_lat=37.3279495&lgl_long=127.0949293&abt=&_callback=viewMoreContents"""
+        URL = {
+            "view": f"""https://s.search.naver.com/p/review/search.naver?rev=44&where=view&api_type=11&start={page}&query={keyword}&nso=so:r,p:from{start_date}to{end_date},a:all&main_q=&mode=normal&q_material=&ac=0&aq=0&spq=0&st_coll=&topic_r_cat=&nx_search_query=&nx_and_query=&nx_sub_query=&sm=tab_opt&ssc=tab.view.view&ngn_country=KR&lgl_rcode=02465101&fgn_region=&fgn_city=&abt=&_callback=viewMoreContents""",
+            "blog": f"""https://s.search.naver.com/p/blog/search.naver?where=blog&sm=tab_pge&api_type=1&query={keyword}&rev=44&start={page}&dup_remove=1&post_blogurl=&post_blogurl_without=&nso=&dkey=0&source_query=&nx_search_query={keyword}&spq=3&_callback=viewMoreContents""",
+            "cafe": f"""https://s.search.naver.com/p/cafe/search.naver?where=article&ie=utf8&query={keyword}&prdtype=0&t=0&st=rel&srchby=text&dup_remove=1&cafe_url=&without_cafe_url=&sm=tab_opt&nso_open=0&rev=44&abuse=0&ac=0&aq=0&converted=0&is_dst=0&nqx_context=&nx_and_query=&nx_search_hlquery=&nx_search_query=&nx_sub_query=&people_sql=0&spq=3&x_tab_article=&is_person=0&start={page}&display=10&prmore=1&_callback=viewMoreContents"""
+        }
+        # print(page)
 
-        data = requests.get(URL)
-        data_dic = json.loads(data.text[18:-4])
+        data = requests.get(URL[where])
+        soup = bs(data.text.strip(), "html.parser")
 
-        soup = bs(data_dic["html"], "html.parser")
-
-        results = soup.findAll("a", {'class': ['api_txt_lines']})
-
-        if len(results) == 0:
-            break
+        results = soup.findAll("a", {'class': ['\\\"api_txt_lines']})
 
         a_tags.extend( results )
+
+        if len(results) <= 1:
+            break
+
         page += 30
 
-    cafes = set()
-    blogs = set()
+    urls = set()
 
     for a in a_tags:
         h = a["href"]
         if h == "#":
             continue
-        elif "cafe.naver.com" in h:
-            cafes.add(h)
-        else:
-            blogs.add(h)
+        urls.add(h)
 
-
-    cafe_urls = list(cafes)
-    blog_urls = list(blogs)
-    result = {
-        "count": len(cafe_urls)+len(blog_urls),
-        "cafe_urls": cafe_urls,
-        "blog_urls": blog_urls
-    }
+    result = list(urls)
 
     return result
