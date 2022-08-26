0bd141c009babacf1620bd261d7b8e4fa8e89d79
Minku Koo <corleone@kakao.com>
2022-08-26 21:48:30 +0900

    create extract.py

---
+++ keywordExtract/extract.py
@@ -0,0 +1,30 @@
+
+
+
+def readRawData(filename):
+    with open('.../rawData/' + filename + '.txt') as f:
+        data = f.read()
+    return data
+
+def mecabProcess(data):
+    # mecab preprocess
+    # only 동사 명사 형용사
+    return 
+
+def getWordFrequency(data):
+    # word freaquency calculation
+    # create graph
+    # create result text file
+    return 
+
+def getWordCloud(data):
+    # create worldcloud
+    return 
+
+def doKeybert():
+    # using keybert model
+    # keyword extraction
+    # make graph
+    # create result text file
+    return 
+
