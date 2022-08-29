cc470f56a4a0f23feea2fa85beb00030bd3c81f8
Minku Koo <corleone@kakao.com>
2022-08-29 20:19:57 +0900

    Update extract.py

--- keywordExtract/extract.py
+++ keywordExtract/extract.py
@@ -1,8 +1,11 @@
 
-
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import nltk
 
 def readRawData(filename):
-    with open('.../rawData/' + filename + '.txt') as f:
+    with open('../output/rawData/' + filename + '.txt') as f:
         data = f.read()
     return data
 
@@ -17,9 +20,22 @@ def getWordFrequency(data):
     # create result text file
     return 
 
-def getWordCloud(data):
+def getWordCloud(data, title, imsize):
     # create worldcloud
+    word_max = 100
+    wordcloud = WordCloud(font_path='./font/BMDOHYEON_ttf.ttf', 
+                            background_color='white',
+                            max_words=word_max,
+                            max_font_size=200,
+                            height=700,
+                            width=900).generate(data)
+    plt.figure(imsize) #이미지 사이즈 지정
+    plt.imshow(wordcloud, interpolation='lanczos') #이미지의 부드럽기 정도
+    plt.axis('off') #x y 축 숫자 제거
+    plt.savefig('../result-graph/word-cloud/'+title+'-wordcloud.png', dpi=300)
     return 
+
+
 
 def doKeybert():
     # using keybert model
